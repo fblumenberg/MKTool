0e4a74568b7a9d4f7d0800758fe332ed78bcc469
frank <frank@frankblumenberg.de>
2012-04-19 22:05:48 +0200

    Clean up and Dropbox support

--- MKTool/ApplicationBase/MKTAppDelegate.m
+++ MKTool/ApplicationBase/MKTAppDelegate.m
@@ -1,19 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2012, Frank Blumenberg
 //
-//  MKTAppDelegate.m
-//  MKTool
+// See License.txt for complete licensing and attribution information.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  Created by Frank Blumenberg on 19.04.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+/////////////////////////////////////////////////////////////////////////////////
 
 #import "MKTAppDelegate.h"
 
 #import "MKTMasterViewController.h"
-
 #import "MKTDetailViewController.h"
 
 #import "BlocksKit.h"
 
+#import "DropboxSDK/DropboxSDK.h"
+
+// Here we import the Dropbox credentials. You have to get your own to compile.
+#import "ExternalData.h"
+
+#ifndef kDROPBOX_APP_KEY
+#define kDROPBOX_APP_KEY @"<YOUR CONSUMER KEY>"
+#endif
+
+#ifndef kDROPBOX_APP_SECRET
+#define kDROPBOX_APP_SECRET @"<YOUR CONSUMER SECRET>"
+#endif
+
+
+@interface MKTAppDelegate () <DBSessionDelegate>
+
+@end
+
+/////////////////////////////////////////////////////////////////////////////////
 
 @implementation MKTAppDelegate
 
@@ -21,58 +55,77 @@
 @synthesize navigationController = _navigationController;
 @synthesize splitViewController = _splitViewController;
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  // Override point for customization after application launch.
   if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-      MKTMasterViewController *masterViewController = [[MKTMasterViewController alloc] initWithNibName:@"MKTMasterViewController_iPhone" bundle:nil];
-      self.navigationController = [[UINavigationController alloc] initWithRootViewController:masterViewController];
-      self.window.rootViewController = self.navigationController;
+    MKTMasterViewController *masterViewController = [[MKTMasterViewController alloc] initWithNibName:@"MKTMasterViewController_iPhone" bundle:nil];
+    self.navigationController = [[UINavigationController alloc] initWithRootViewController:masterViewController];
+    self.window.rootViewController = self.navigationController;
   } else {
-      MKTMasterViewController *masterViewController = [[MKTMasterViewController alloc] initWithNibName:@"MKTMasterViewController_iPad" bundle:nil];
-      UINavigationController *masterNavigationController = [[UINavigationController alloc] initWithRootViewController:masterViewController];
-      
-      MKTDetailViewController *detailViewController = [[MKTDetailViewController alloc] initWithNibName:@"MKTDetailViewController_iPad" bundle:nil];
-      UINavigationController *detailNavigationController = [[UINavigationController alloc] initWithRootViewController:detailViewController];
-  	
-  	masterViewController.detailViewController = detailViewController;
-  	
-      self.splitViewController = [[UISplitViewController alloc] init];
-      self.splitViewController.delegate = detailViewController;
-      self.splitViewController.viewControllers = [NSArray arrayWithObjects:masterNavigationController, detailNavigationController, nil];
-      
-      self.window.rootViewController = self.splitViewController;
+    MKTMasterViewController *masterViewController = [[MKTMasterViewController alloc] initWithNibName:@"MKTMasterViewController_iPad" bundle:nil];
+    UINavigationController *masterNavigationController = [[UINavigationController alloc] initWithRootViewController:masterViewController];
+
+    MKTDetailViewController *detailViewController = [[MKTDetailViewController alloc] initWithNibName:@"MKTDetailViewController_iPad" bundle:nil];
+    UINavigationController *detailNavigationController = [[UINavigationController alloc] initWithRootViewController:detailViewController];
+
+    masterViewController.detailViewController = detailViewController;
+
+    self.splitViewController = [[UISplitViewController alloc] init];
+    self.splitViewController.delegate = detailViewController;
+    self.splitViewController.viewControllers = [NSArray arrayWithObjects:masterNavigationController, detailNavigationController, nil];
+
+    self.window.rootViewController = self.splitViewController;
   }
-    [self.window makeKeyAndVisible];
-    return YES;
+  [self.window makeKeyAndVisible];
+
+  // Initialize the Dropbox support
+  // Set these variables before launching the app
+  NSString *appKey = kDROPBOX_APP_KEY;
+  NSString *appSecret = kDROPBOX_APP_SECRET;
+  NSString *root = kDBRootAppFolder;
+
+  DBSession *session = [[DBSession alloc] initWithAppKey:appKey appSecret:appSecret root:root];
+  session.delegate = self;
+  [DBSession setSharedSession:session];
+
+  return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+  if ([[DBSession sharedSession] handleOpenURL:url]) {
+    return YES;
+  }
+
+  return NO;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
   // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
   // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
   // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
   // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
   // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - DBSessionDelegate methods
+
+- (void)sessionDidReceiveAuthorizationFailure:(DBSession *)session userId:(NSString *)userId {
+  [[DBSession sharedSession] linkUserId:userId];
 }
 
 @end
